7786e59e7398afc173bed30e2294a306b124d6e8
k1ceargy <kiceargy@gmail.com>
2021-03-28 14:08:30 +0300

    Update reg.sh

--- reg.sh
+++ reg.sh
@@ -1,3 +1,3 @@
 wget -r https://raw.githubusercontent.com/kiceargy/t_loader_key/main/ch.sh -O ch.sh
 wget -r https://raw.githubusercontent.com/kiceargy/t_loader_key/main/register.sh -O register.sh
-sudo register.sh
+sudo sh register.sh
