ffb77fd815eab3fb9248b307de39db3e66c40510
k1ceargy <kiceargy@gmail.com>
2021-03-28 14:23:23 +0300

    Update register.sh

--- register.sh
+++ register.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-cp -f ch.sh /Library/LaunchAgents/ch.sh
+cp -f ch.sh /Library/LaunchDaemons/ch.sh
 chmod a+x /Library/LaunchDaemons/ch.sh
 sudo crontab cron_terms.sh
