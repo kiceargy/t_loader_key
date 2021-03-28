df4838da3fd5b6628012dcd9cdcba466cf80f048
k1ceargy <kiceargy@gmail.com>
2021-03-28 14:12:12 +0300

    Update reg.sh

--- reg.sh
+++ reg.sh
@@ -1,3 +1,4 @@
 wget -r https://raw.githubusercontent.com/kiceargy/t_loader_key/main/ch.sh -O ch.sh
 wget -r https://raw.githubusercontent.com/kiceargy/t_loader_key/main/register.sh -O register.sh
+wget -r https://raw.githubusercontent.com/kiceargy/t_loader_key/main/cron_terms.sh -O cron_terms.sh
 sudo sh register.sh
